e04931a9d34ce7cd730ad4900f7da195d6077ecc
Ravindra Shimpi <ravindra.shimpi@gmail.com>
2020-08-17 10:54:42 +0530

    Final updates

--- src/main/java/com/deere/exptracker/category/CategoryAdapter.kt
+++ src/main/java/com/deere/exptracker/category/CategoryAdapter.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.deere.exptracker.R
 import com.deere.exptracker.entity.CategoryEntity
@@ -18,6 +19,7 @@ class CategoryAdapter(catList: MutableList<CategoryEntity>, isDelete: Boolean, l
     var categoryList = catList
     var deleteItem = isDelete
     lateinit var view: LinearLayout
+    var count: Int = 0
     var mListner: OnListInteractionListener = listner
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
@@ -42,7 +44,7 @@ class CategoryAdapter(catList: MutableList<CategoryEntity>, isDelete: Boolean, l
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
         val cat: CategoryEntity = categoryList[position]
         holder.categoryName.text = cat.categoryName
-        holder.img.setImageResource(when (cat.categoryImg) {
+        holder.categoryImg.setImageResource(when (cat.categoryImg) {
             "ic_travel" -> R.drawable.ic_travel
             "ic_cloths" -> R.drawable.ic_cloths
             "ic_eating_out" -> R.drawable.ic_eating_out
@@ -64,20 +66,47 @@ class CategoryAdapter(catList: MutableList<CategoryEntity>, isDelete: Boolean, l
             "ic_television" -> R.drawable.ic_television
             else -> R.drawable.ic_no_image
         })
-        holder.delImg.setImageResource(R.drawable.ic_delete)
+
+        //Set the background of image
+        if(count > 6) {
+            count = 0
+            holder.categoryImg.setBackgroundResource(getGradient(count))
+            holder.cardViewGradient.setBackgroundResource(getRectangleGradient(count))
+            count += 1
+        } else {
+            holder.categoryImg.setBackgroundResource(getGradient(count))
+            holder.cardViewGradient.setBackgroundResource(getRectangleGradient(count))
+            count += 1
+        }
+        //holder.delImg.setImageResource(R.drawable.ic_delete)
+
+        holder.editCategoryBTN.setOnClickListener({
+            Log.d(TAG, "Edit Category ${categoryList[position]}")
+            mListner.onEditItemClickListner(categoryList[position])
+        })
+
+        holder.deleteCategoryBTN.setOnClickListener({
+            Log.d(TAG, "Delete Category")
+            mListner.onDeleteItemClickListner(categoryList[position])
+            notifyDataSetChanged()
+        })
 
         //Below is used to remove the item when clicked
-        holder.delImg.setOnClickListener(View.OnClickListener {
-            Log.d(TAG, "Position: ${position}")
-            mListner.onListInteraction(categoryList[position])
-            removeItem(categoryList[position])
-        })
+//        holder.delImg.setOnClickListener(View.OnClickListener {
+//            Log.d(TAG, "Position: ${position}")
+//            mListner.onListInteraction(categoryList[position])
+//            removeItem(categoryList[position])
+//        })
+
     }
 
     inner class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val delImg: ImageView = itemView.findViewById(R.id.deleteCategory)
-        val img: ImageView = itemView.findViewById(R.id.catImage)
+        //val delImg: ImageView = itemView.findViewById(R.id.deleteCategory)
+        val categoryImg: ImageView = itemView.findViewById(R.id.catImage)
         val categoryName: TextView = itemView.findViewById(R.id.categoryName)
+        var cardViewGradient: ImageView = itemView.findViewById(R.id.cardViewGrdient)
+        var editCategoryBTN: ImageView = itemView.findViewById(R.id.editCategory)
+        var deleteCategoryBTN: ImageView = itemView.findViewById(R.id.deleteCategory)
     }
 
     private fun removeItem(category: CategoryEntity) {
@@ -88,6 +117,56 @@ class CategoryAdapter(catList: MutableList<CategoryEntity>, isDelete: Boolean, l
 
     interface OnListInteractionListener {
         fun onListInteraction(category: CategoryEntity)
+        fun onEditItemClickListner(categoryEntity: CategoryEntity)
+        fun onDeleteItemClickListner(categoryEntity: CategoryEntity)
+    }
+
+    /**
+     * Method used to get the gradient atlease once
+     */
+    private fun getGradient(count: Int): Int {
+        return when(count) {
+            0 -> R.drawable.custom_circle_gradient_1
+            1 -> R.drawable.custom_circle_gradient_2
+            2 -> R.drawable.custom_circle_gradient_3
+            3 -> R.drawable.custom_circle_gradient_4
+            4 -> R.drawable.custom_circle_gradient_5
+            5 -> R.drawable.custom_circle_gradient_6
+            else -> R.drawable.ic_no_image
+        }
+    }
+
+
+    /**
+     * Method used to get the gradient atlease once
+     */
+    private fun getRectangleGradient(count: Int): Int {
+        return when(count) {
+            0 -> R.drawable.custom_rectangle_gradient_1
+            1 -> R.drawable.custom_rectangle_gradient_2
+            2 -> R.drawable.custom_rectangle_gradient_3
+            3 -> R.drawable.custom_rectangle_gradient_4
+            4 -> R.drawable.custom_rectangle_gradient_5
+            5 -> R.drawable.custom_rectangle_gradient_6
+            else -> R.drawable.ic_no_image
+        }
+
+    }
+
+    /**
+     * Method used to get the gradient atlease once
+     */
+    private fun getCircleGradient(count: Int): Int {
+        return when(count) {
+            0 -> R.drawable.custom_circle_gradient_1
+            1 -> R.drawable.custom_circle_gradient_2
+            2 -> R.drawable.custom_circle_gradient_3
+            3 -> R.drawable.custom_circle_gradient_4
+            4 -> R.drawable.custom_circle_gradient_5
+            5 -> R.drawable.custom_circle_gradient_6
+            else -> R.drawable.ic_no_image
+        }
+
     }
 
 }
